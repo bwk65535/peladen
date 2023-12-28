3d055f22cbd6a9f003b8f97c5be315d970bf150b
Mochamad Bayu <bwk65535@gmail.com>
2023-12-28 14:17:39 +0000

    add simpel theme

--- ch_img_url.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -exo pipefail
-
-grep -rl '/assets/images' _posts/ | xargs sed -i 's/\/assets\/images/https:\/\/res.cloudinary.com\/peladen\/image\/upload\/v1612739828\/peladen/g' > /dev/null
